cedb2f6c65f6a47ea6f7bd57a15ed7df3a0e8969
DHR <dave@dhrapson.com>
2015-11-30 11:41:55 +0000

    make run_specs.sh work from any dir

--- run_specs.sh
+++ run_specs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-set -e
+echo $PWD
+pushd `dirname $0`
+	set -e
 
-pushd system_tests
-	bundle
-	bundle exec rspec
+	pushd system_tests
+		bundle
+		bundle exec rspec
+	popd
 popd
 
 exit 0
