07f486f64782c0ffa391ad638de09e76e2586190
DHR <dave@dhrapson.com>
2015-12-01 15:51:48 +0000

    turn on s3 deployment in Travis CI

--- run_specs.sh
+++ run_specs.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-echo "before $PATH"
 PATH=$PATH:$GOPATH/bin
-echo "after $PATH"
 
 echo $PWD
 pushd `dirname $0`
 	set -e
-
-	pushd resemble
-		go install
-	popd
 
 	pushd system_tests
 		bundle
